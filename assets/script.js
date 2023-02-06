df7f24dfed5f5e3fc6cc920f655074c953ca16c7
Suhailah de Moraes <suh.alvesmoraes@gmail.com>
2023-02-06 21:19:13 +0000

    Challenge - Blog

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+const icon = document.querySelectorAll("#icon");
+  icon.forEach((icon) => {
+    icon.onclick = () => {
+      icon.classList.toggle("filled");
+    };
+  });
+
+console.log(icon.onclick);
